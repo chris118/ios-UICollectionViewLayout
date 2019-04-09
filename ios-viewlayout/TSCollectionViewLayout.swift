bfe576677d2d953d7067160b0e266a4a223145aa
xiaopeng <12405301@qq.com>
2019-04-09 16:42:49 +0800

    update tap handle

--- ios-viewlayout/TSCollectionViewLayout.swift
+++ ios-viewlayout/TSCollectionViewLayout.swift
@@ -36,6 +36,47 @@ class TSCollectionViewLayout: UICollectionViewFlowLayout {
     
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
         // TODO
+//        let collectionViewWidth = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: <#T##CGPoint#>)
         return super.targetContentOffset(forProposedContentOffset: proposedContentOffset)
+    }
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        
+        guard let _collectionView = collectionView else {
+            return CGPoint(x: 0, y: 0)
+        }
+        
+        let collectionWidth = self.collectionView?.bounds.size.width ?? CGFloat(0)
+
+        // 最终偏移量
+        var targetPoint = super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
+        
+        // 获取最终显示的区域
+        let targetRect = CGRect(x: targetPoint.x, y: CGFloat(0), width: collectionWidth, height: CGFloat(MAXFLOAT))
+
+        // 获取最终显示的cell
+        let attrs = self.layoutAttributesForElements(in: targetRect)
+        
+        guard let _attrs = attrs else {
+            return targetPoint
+        }
+        
+        var minDelta = CGFloat(MAXFLOAT)
+        for attr in _attrs {
+            let delta = (attr.center.x - targetPoint.x) - _collectionView.bounds.size.width * 0.5
+            if abs(delta) < abs(minDelta) {
+                minDelta = delta
+            }
+        }
+        
+        // 移动间距
+        targetPoint.x += minDelta;
+        
+        if targetPoint.x < 0 {
+            targetPoint.x = 0;
+        }
+        
+        return targetPoint;
+        
     }
 }
